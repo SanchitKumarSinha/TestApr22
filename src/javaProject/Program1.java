9c9b03b3f45a0c06e4dceec3c9161a42571fc000
Sanchit kumar <sanchit77kumar@gmail.com>
2022-07-29 08:17:23 +0530

    main method change

--- src/javaProject/Program1.java
+++ src/javaProject/Program1.java
@@ -3,6 +3,8 @@ package javaProject;
 public class Program1 {
 
 	public static void main(String[] args) {
-		System.out.println("Project Upload");
+		System.out.println("Project Upload Branch A");
+		System.out.println("Project Upload........A");
+		System.out.println("Project Upload--------A");
 	}
 }
