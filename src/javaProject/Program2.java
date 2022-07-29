f33acc8cbd21bda187e4c4a897afb4c3be9ea03f
Sanchit kumar <sanchit77kumar@gmail.com>
2022-07-29 08:53:14 +0530

    Branch B2

--- src/javaProject/Program2.java
+++ src/javaProject/Program2.java
@@ -6,5 +6,7 @@ public class Program2 {
 		System.out.println("Project Upload Part 1-2");
 		System.out.println("Project Upload Branch B1");
 		System.out.println("Project Upload ------ B1");
+		System.out.println("Project Upload Branch B2-1");
+		System.out.println("Project Upload ------ B2-1");
 	}
 }
